6da4ef8a7a4167264db9f4479b727d766efad0a6
Esteban <estebanfabiancorona@gmail.com>
2022-05-25 07:56:37 -0700

    comentario

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+$router->get('/version', function () use ($router) {
+
+    return $route->app->version();
+});
+
+$router->get('/saludo/{nombre}', function($nombre) {
+    return 'Hola mi nombre es '. $nombre;
+});
+
+$router->get('/calcular/{edad}', function($edad) {
+    if($edad>0 && $edad <18)
+        return 'eres menor de edad '. $edad;
+    } elseif ($edad >= 18 && $edad <= 100) {
+         return 'eres mayor de edad':
+    } else {
+    return 'edad erronea' ;
+    }
+});
+
+$router->post('/saludo', function () {
+    return 'SE CREO UN DATO ';
 });
